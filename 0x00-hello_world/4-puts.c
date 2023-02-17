eebabf8d45dfa981de71cc45e7db4c0fa1d82cf2
Miracle Samuel <bidemimiracle@gmail.com>
2023-02-16 21:14:13 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followd by a newline
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline
  * Return: Always  0.
  */
 
