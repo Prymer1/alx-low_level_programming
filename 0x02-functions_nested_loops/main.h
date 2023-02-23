64bb7d7e905228fcd0f287ea6d96646bda793379
Miracle Samuel <bidemimiracle@gmail.com>
2023-02-23 09:19:41 -0800

    Task 7

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,7 @@
  * for prototype functions used in the 0x02-functions_nested_loops*/
 
 int main(void);
-Int _putchar(int);
+int _putchar(int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);int _isalpha(int c);
