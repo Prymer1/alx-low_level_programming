63ec7eca7ad088ae75d7954ba0903f34268a18a8
Miracle Samuel <bidemimiracle@gmail.com>
2023-03-01 00:58:24 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,8 +3,7 @@
  * Return: 0
  */
 
-int main(void);
-
+int main(void)
 {
 	int i;
 
@@ -14,9 +13,7 @@ int main(void);
  * i = 0;
  *
  * while (i < 10)
- * {
- *	putchar(i);
- * }
+ * { putchar(i); }
  */
 
 	printf("Infinite loop avoided! \\o/\n");
