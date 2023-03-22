542e3efcf8c7c38856c99f8ed79d94524e725e65
Miracle Samuel <bidemimiracle@gmail.com>
2023-03-21 21:34:30 -0700

    Task 1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,7 +3,8 @@
 
 /*
  * File: 1-pi.h
- * Desc: Header file that defines a macro named PI as an abbreviation for the token 3.14159265359.
+ * Desc: Header file that defines a macro named PI
+ * as an abbreviation for the token 3.14159265359.
  * Headerfile guarded to avoid problem of double inclusion.
  */
 
