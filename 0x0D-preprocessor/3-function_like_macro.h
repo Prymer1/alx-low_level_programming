5be4a316d6092f1d307f6ccf3b1efd6218a31492
Miracle Samuel <bidemimiracle@gmail.com>
2023-03-21 21:39:13 -0700

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,8 @@
 
 /*
  * File: 3-function_like_macro.h
- * Desc: Header file that defines a function-like macro ABS(x) that computes the absolute value of a number x.
+ * Desc: Header file that defines a function-like macro ABS(x)
+ * that computes the absolute value of a number x.
  * Header file guarded to avoid the problem of double inclusion.
  */
 
