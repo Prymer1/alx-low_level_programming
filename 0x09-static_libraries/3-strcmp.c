f8337a881bbed843fb2627d96efe4d679e8f53aa
Prymer1 <bidemimiracle@gmail.com>
2024-08-13 07:12:47 -0700

    Task 1

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
+
+/**
+ * _strcmp - Compares 2 string ending at first different byte
+ * @s1: pointer to first string.
+ * @s2: pointer to second string.
+ *
+ * Return: If str1 < str2, the negative difference
+ *         If str1 == str2, 0.
+ *         If str1 > str2, the positive difference
+ */
+
+
